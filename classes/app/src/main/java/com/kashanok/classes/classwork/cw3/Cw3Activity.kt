ecaabbc20cfafa0ef4260f659a1932bad13ce2ad
kashanok <r.kashanok@softteco.com>
2019-02-26 20:36:43 +0300

    login screen styles changed

---
+++ classes/app/src/main/java/com/kashanok/classes/classwork/cw3/Cw3Activity.kt
@@ -0,0 +1,13 @@
+package com.kashanok.classes.classwork.cw3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kashanok.classes.R
+
+class Cw3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cw3)
+    }
+}

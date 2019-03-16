f7a187eba0c2c2856c02d1415ca4cba176f35b4f
kashanok <r.kashanok@softteco.com>
2019-03-16 13:21:28 +0300

    hw6: internet connection check added

--- classes/app/src/main/java/com/kashanok/classes/homework/common/HomeworkClickListener.kt
+++ classes/app/src/main/java/com/kashanok/classes/homework/common/HomeworkClickListener.kt
@@ -1,6 +1,8 @@
 package com.kashanok.classes.homework.common
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.widget.Toast
 import com.kashanok.classes.common.BaseAdapterItem
 import com.kashanok.classes.common.BaseItemClickListener
 import com.kashanok.classes.homework.common.workitem.HomeWorkNumber
@@ -11,6 +13,7 @@ import com.kashanok.classes.homework.hw3.LoadPictureActivity
 import com.kashanok.classes.homework.hw4.Hw4Activity
 import com.kashanok.classes.homework.hw5.Hw5Activity
 import com.kashanok.classes.homework.hw6.Hw6MainActivity
+
 
 class HomeworkClickListener(val context: Context) : BaseItemClickListener<BaseAdapterItem<WorkNumber>> {
 
@@ -33,8 +36,18 @@ class HomeworkClickListener(val context: Context) : BaseItemClickListener<BaseAd
                 context.startActivity(Hw5Activity.getIntent(context))
             }
             HomeWorkNumber.SIX -> {
-                context.startActivity(Hw6MainActivity.getIntent(context))
+                if(isNetworkConnected()){
+                    context.startActivity(Hw6MainActivity.getIntent(context))
+                } else {
+                    Toast.makeText(context, "CHECK YOUR INTERNET CONNECTION", Toast.LENGTH_LONG).show()
+                }
             }
         }
+    }
+
+    private fun isNetworkConnected(): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo
+        return activeNetwork?.isConnected ?: false
     }
 }
--- classes/app/src/main/java/com/kashanok/classes/homework/hw6/Hw6MainActivity.kt
+++ classes/app/src/main/java/com/kashanok/classes/homework/hw6/Hw6MainActivity.kt
@@ -5,10 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import com.kashanok.classes.R
 import com.kashanok.classes.common.ItemOffsetDecoration
 import com.kashanok.classes.homework.hw6.recycler.Hw6RvAdapter
 import kotlinx.android.synthetic.main.activity_hw6.*
+import com.kashanok.classes.R.layout.*
 
 class Hw6MainActivity : AppCompatActivity() {
 
@@ -23,7 +23,7 @@ class Hw6MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hw6)
+        setContentView(activity_hw6)
 
         rvStudents.layoutManager = LinearLayoutManager(this)
 
@@ -43,12 +43,12 @@ class Hw6MainActivity : AppCompatActivity() {
     private fun getItemDecoration(): ItemOffsetDecoration {
         return ItemOffsetDecoration(
             this,
-            R.dimen.dimen_27dp,
-            R.dimen.dimen_9dp,
-            R.dimen.dimen_9dp,
-            R.dimen.dimen_9dp,
-            R.dimen.dimen_0dp,
-            R.dimen.dimen_27dp
+            com.kashanok.classes.R.dimen.dimen_27dp,
+            com.kashanok.classes.R.dimen.dimen_9dp,
+            com.kashanok.classes.R.dimen.dimen_9dp,
+            com.kashanok.classes.R.dimen.dimen_9dp,
+            com.kashanok.classes.R.dimen.dimen_0dp,
+            com.kashanok.classes.R.dimen.dimen_27dp
         )
     }
 }
